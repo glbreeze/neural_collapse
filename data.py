6e24709939b530edb839916972173df216c37a03
glbreeze <glbreeze@gmail.com>
2023-08-25 18:17:06 +0800

    stl10

--- data.py
+++ data.py
@@ -14,16 +14,15 @@ from torchvision import datasets, transforms
 
 def get_dataloader(args):
 
-    if args.dset == 'cifar10':
-        mean = [0.4914, 0.4822, 0.4465]
-        std = [0.2470, 0.2434, 0.2615]
-    if args.dset == 'cifar100':
-        mean = [0.5071, 0.4865, 0.4409]
-        std = [0.2673, 0.2564, 0.2761]
+    if args.dset in ['cifar10' 'stl10']:
+        normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2434, 0.2615])
+    elif args.dset == 'cifar100':
+        normalize = transforms.Normalize(mean=[0.5071, 0.4865, 0.4409], std=[0.2673, 0.2564, 0.2761])
 
     transform = transforms.Compose([
+        # transforms.RandomCrop(96, padding=4), # for stl10
         transforms.ToTensor(),
-        transforms.Normalize(mean, std)
+        normalize
     ])
 
     if args.dset == 'cifar10':
@@ -41,6 +40,15 @@ def get_dataloader(args):
 
         test_loader = torch.utils.data.DataLoader(
             datasets.CIFAR100('data', train=False, download=True, transform=transform),
+            batch_size=args.batch_size, shuffle=False)
+
+    elif args.dset == 'stl10':
+        # print('STL10 is on the test')
+        trainloader = torch.utils.data.DataLoader(
+            datasets.STL10('data', split='train', download=True, transform=transform),
+            batch_size=args.batch_size, shuffle=True)
+        testloader = torch.utils.data.DataLoader(
+            datasets.STL10('data', split='test', download=True, transform=transform),
             batch_size=args.batch_size, shuffle=False)
 
     return train_loader, test_loader
